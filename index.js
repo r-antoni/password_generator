3699cda695d9b1bec50836c40eaa3722991371ac
R-Antoni <reindraantoni@gmail.com>
2023-01-09 08:21:03 +0700

    Updated JS

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-
-let randomNum = Math.floor(Math.random() * characters.length)
+let passLength = 15
 let password1 = document.querySelector(".password1")
 let password2 = document.querySelector(".password2")
+let isGenerated = false
+
+
+
 
 document.querySelector("#generate").addEventListener("click", getRandomPass)
 
 function getRandomPass(){
-    randomPassword1()
+    if(isGenerated === false){
+        randomPassword1()
+        randomPassword2()
+    } else {
+        alert("Refresh browser to get new password")
+    }
+    isGenerated = true
+}
 
+function randomChar(){
+    let randomNum = Math.floor(Math.random() * characters.length)
+    return characters[randomNum]
 }
 
 function randomPassword1(){
-    for(let i = 0; i < 13; i++){
-        password1.textContent += characters[randomNum]
+    for(let i = 0; i < passLength; i++){
+        password1.textContent += randomChar()
     }
 }
 
 function randomPassword2(){
-    for(let i = 0; i < 13; i++){
-        password2.textContent += characters[randomNum]
+    for(let i = 0; i < passLength; i++){
+        password2.textContent += randomChar()
     }
 }
