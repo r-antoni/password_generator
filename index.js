f5816f3764702282fe684ad31b8b73fba5aa0552
R-Antoni <reindraantoni@gmail.com>
2023-01-10 11:47:27 +0700

    V1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passLength = 15
+let length = 16;
 let password1 = document.querySelector(".password1")
 let password2 = document.querySelector(".password2")
 let isGenerated = false
@@ -26,13 +26,13 @@ function randomChar(){
 }
 
 function randomPassword1(){
-    for(let i = 0; i < passLength; i++){
+    for(let i = 0; i < length; i++){
         password1.textContent += randomChar()
     }
 }
 
 function randomPassword2(){
-    for(let i = 0; i < passLength; i++){
+    for(let i = 0; i < length; i++){
         password2.textContent += randomChar()
     }
 }
